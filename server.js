afc31e7b0cc5e45cc07fa02f8e0a7b1ba922a4de
ThomasBranchaud <tabranchaud@wpi.edu>
2026-03-30 17:37:51 -0400

    connection point for sending player info

--- server.js
+++ server.js
@@ -13,9 +13,9 @@ app.get('/testConnection', (req, res) => {
     res.send('Test connection successful');
 })
 
-app.post('/sendUserInfo', (req, res) => {
-    const {username, info} = req.body;
-    console.log(info);
+app.post('/sendPlayerProfile', (req, res) => {
+    const {username, score} = req.body;
+    console.log(score);
     console.log(username);
     res.send('Received user: ' + username);
 })
