41413c160e632dad1f5d6a4628c218d55450215c
ThomasBranchaud <tabranchaud@wpi.edu>
2026-04-05 22:44:33 -0400

    get player profile

--- server.js
+++ server.js
@@ -13,6 +13,25 @@ app.get('/testConnection', (req, res) => {
     res.send('Test connection successful');
 })
 
+app.get('/get-user', (req , res) => {
+    const username = req.query.username;
+    if (!username){
+        console.log('Username not inputted correctly');
+        return res.send('Username not inputted correctly');
+    }
+
+    const fakeDB = {
+        Joe:{username: "Joe", score:100},
+        Jeff:{username:"Jeff", score:200}
+    };
+
+    const player = fakeDB[username];
+    if (!player){
+        res.send("User not found");
+    }
+    res.json(player);
+})
+
 app.post('/sendPlayerProfile', (req, res) => {
     const {username, score} = req.body;
     console.log(score);
